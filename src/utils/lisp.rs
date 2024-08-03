955f2bea88fe8e94e5ae87e09ae6140d5c30b5f5
Vincent S. <vs38190@gmail.com>
2024-08-04 00:35:49 +0200

    Create a lisp file holding some usefull macro

---
+++ src/utils/lisp.rs
@@ -0,0 +1,44 @@
+macro_rules! prog1 {
+    ($first:expr; $($rest:expr);*;) => {{
+        let __prog1_result = $first;
+        $(
+            $rest;
+        )*
+        __prog1_result
+    }};
+}
+
+macro_rules! prog2 {
+    ($first:expr; $($rest:expr);*;) => {{
+        $first;
+        prog1!($($rest);*;)
+    }};
+}
+
+// macro_rules! prog2 {
+//     ($first:expr; $($rest:expr);*) => {{
+//         $first;
+//         prog1!($($rest);*)
+//     }};
+// }
+
+pub(crate) use prog1;
+pub(crate) use prog2;
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prog1() {
+        let result = prog1!(1; 2; 3; 4; 5;);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_prog2() {
+        let result = prog2!(1; 2; 3; 4; 5;);
+        assert_eq!(result, 2);
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,4 @@
 mod circular_buffer;
 
+pub mod lisp;
 pub use circular_buffer::CircularBuffer;
